27cc6ad81e27dcc416b1ae8b31eefe82778de264
Ben Howes <ben@ben-howes.co.uk>
2022-02-08 10:11:25 +0000

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="django-monthfield",
-    version="1.0.1",
+    version="1.0.2",
     author="Matthys Kroon",
     author_email="matthysk@clearspark.co.za",
     packages=["month"],
